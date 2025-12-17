f4baae2678f2c75583c7ae34e4f101c8b3b426d9
Alexei2006-m <lehaa6505@gmail.com>
2025-12-17 20:18:41 +0300

    Create task2.cpp

---
+++ Task2/task2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+  double x1, y1, x2, y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+  
+  double distance = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+  
+  cout << fixed << setprecision(8) << distance << endl;
+  return 0; 
+}
