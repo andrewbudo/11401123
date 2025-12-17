b25a7c6a1db39130001781306575077b7f5b6e2c
r0maNk0pcev1ch <romankopcevic@gmail.com>
2025-12-17 20:04:10 +0300

    Create 1.cpp

---
+++ Task1/1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() {
+    double a = 3;
+    double b = 5;
+    int area, perimeter; 
+    area = a * b;
+    perimeter = 2 * (a + b);
+    printf("%d %d\n", area, perimeter);
+
+    return 0; 
+}
